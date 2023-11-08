cbf5d56dc40f319bf46ce4f61650f5b8eaa21752
Fatbardh Durmishi <fdurmishi789@gmail.com>
2023-11-08 14:48:53 +0100

    finnished with sequelize

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 // function requestListener(req, res) {
 
@@ -34,15 +40,51 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+//!this is just registered as a middleware and will not execute before the sequelize, but only for incoming requests
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Bardh", email: "fdurmishi789@gmail.com" });
+    }
+    //*if u return a value in a then block it is automatically wrapped into a new promise
+    return user;
+    //*if u need to return a promise u do Promise.resolve(user)
+  })
+  .then((user) => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -15,17 +15,35 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  const userId = req.user.id;
+  // *createProduct is a special method created by sequelize when we create associations(relations between models/tables)
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      userId: userId,
+    })
     .then((result) => {
       console.log("Created Product");
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
+
+  //!before we created relations
+  // Product.create({
+  //   title: title,
+  //   price: price,
+  //   imageUrl: imageUrl,
+  //   description: description,
+  //   userId: userId,
+  // })
+  //   .then((result) => {
+  //     console.log("Created Product");
+  //     res.redirect("/admin/products");
+  //   })
+  //   .catch((err) => console.log(err));
 
   //!oldway
   // const product = new Product(null, title, imageUrl, description, price);
@@ -45,14 +63,15 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then((products) => {
       res.render("admin/edit-product", {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product,
+        product: products[0],
         buttonText: "Update Product",
       });
     })
@@ -104,7 +123,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
